a1ff7e0b7f582b9cdb93d87c7a867b971f0d8e12
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2017-01-29 16:10:32 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -5,10 +5,8 @@ cp /defaults/* /etc/postfix/
 echo -e $MYHOSTNAME$MYNETWORKS$RELAYDOMAINS >> /etc/postfix/main.cf
 echo -e smtpd_tls_cert_file = $CERT >> /etc/postfix/main.cf
 echo -e smtpd_tls_key_file = $KEY >> /etc/postfix/main.cf
-echo -e smtpd_tls_CApath = $CA >> /etc/postfix/main.cf
 echo -e smtp_tls_cert_file = $CERT >> /etc/postfix/main.cf
 echo -e smtp_tls_key_file = $KEY >> /etc/postfix/main.cf
-echo -e smtp_tls_CApath = $CA >> /etc/postfix/main.cf
 echo -e $TRANSPORT > /etc/postfix/transport
 postmap /etc/postfix/transport
 touch /etc/aliases
