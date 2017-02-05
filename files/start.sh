a0bc11356ed026270df1a9113e2c455529c06dcd
amuehlbauer <andreas.muehlbauer@ianus-it.de>
2017-02-05 15:59:17 +0100

    Update start.sh

--- files/start.sh
+++ files/start.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 rsyslogd
 rm /etc/postfix/main.cf
+rm /etc/aliases* /etc/postfix/transport*
 cp /defaults/* /etc/postfix/
-echo -e $MYHOSTNAME$MYNETWORKS$RELAYDOMAINS >> /etc/postfix/main.cf
-echo -e smtpd_tls_cert_file = $CERT >> /etc/postfix/main.cf
-echo -e smtpd_tls_key_file = $KEY >> /etc/postfix/main.cf
-echo -e smtp_tls_cert_file = $CERT >> /etc/postfix/main.cf
-echo -e smtp_tls_key_file = $KEY >> /etc/postfix/main.cf
+sed -i "s/SERVICENAME/${SERVICENAME}/g" /etc/postfix/main.cf
+echo -e $MYNETWORKS$RELAYDOMAINS >> /etc/postfix/main.cf
 echo -e $TRANSPORT > /etc/postfix/transport
 postmap /etc/postfix/transport
 touch /etc/aliases
